7cb254e00d3446282539629cd700a10a894d475e
veena-sridhar <v.sridhar91@gmail.com>
2016-05-06 19:43:33 -0700

    pass videoPlayer specs

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,15 +1,51 @@
-var App = () => (
-  <div>
-    <Nav />
-    <div className="col-md-7">
-      <VideoPlayer/>
-    </div>
-    <div className="col-md-5">
-      <VideoList/>
-    </div>
-  </div>
-);
+// var App = () => (
+//   <div>
+//     <Nav />
+//     <div className="col-md-7">
+//       <VideoPlayer/>
+//     </div>
+//     <div className="col-md-5">
+//       <VideoList/>
+//     </div>
+//   </div>
+// );
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      //set the state for - 
+      // all the videos in the video list
+
+      //current video in the player
+
+    };
+
+    handleClick = function(event) {
+     // this.setState;
+    };
+
+    //handleClick.bind(reference to videoPlayer, reference to videoList)
+
+  }
+
+  render() {
+    return (
+      <div>
+        <Nav />
+        <div className="col-md-7">
+          <VideoPlayer/>
+        </div>
+        <div className="col-md-5">
+          <VideoList/>
+        </div>
+      </div>
+    );
+  }
+}
 
 // In the ES6 spec, files are "modules" and do not share a top-level scope
 // `var` declarations will only exist globally where explicitly defined
 window.App = App;
+
