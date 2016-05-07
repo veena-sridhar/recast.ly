93200fb770958ee65d043c5940a8cf733c68bd19
veena-sridhar <v.sridhar91@gmail.com>
2016-05-06 17:39:10 -0700

    pass specs for video list and video list entry

--- src/components/VideoList.jsx
+++ src/components/VideoList.jsx
@@ -1,16 +1,28 @@
-var VideoList = () => (
-  <div className="video-list media">
-    <VideoListEntry />
-    <VideoListEntry />
-    <VideoListEntry />
-    <VideoListEntry />
-    <VideoListEntry />
-    <VideoListEntry />
-    <VideoListEntry />
-    <VideoListEntry />
-    <VideoListEntry />
-  </div>
-);
+var VideoList = (props) => { 
+  var list = props.videos.map( (video) => {
+    return <VideoListEntry>{video}</VideoListEntry>; 
+  });
+   
+  return (
+    <div className="video-list media">
+      {list}
+    </div>
+  );  
+};
+
+// class VideoList extends React.Component {
+//   constructor(props) {
+//     super(props);
+//   }
+
+//   render() {
+//     var i = 1;
+//     var list = this.props.videos.map( (video) => {
+//       return <VideoListEntry>{video}</VideoListEntry>; 
+//     });
+//     return <div className="video-list media"> {list} </div>;
+//   }
+// }
 
 // PropTypes tell other developers what `props` a component expects
 // Warnings will be shown in the console when the defined rules are violated
@@ -21,3 +33,5 @@ VideoList.propTypes = {
 // In the ES6 spec, files are "modules" and do not share a top-level scope.
 // `var` declarations will only exist globally where explicitly defined.
 window.VideoList = VideoList;
+
+//React.render(<VideoList videos={exampleVideoData} />, document.getElem('body'));
